58ce03162e667c71db290a0f565745a6b63f510e
Ricardo Sandez <ricardosandez67@gmail.com>
2023-10-16 00:48:24 +0200

    Added tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,7 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-
-describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'clips'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('clips');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('clips app is running!');
+// Jasmine exports a set of global functions to perform tests: describe, it, expect, etc.
+// These can be used only on .spec files
+describe('App Component', () => {
+  it('should pass sanity test', () => {
+    expect(true).toBeTruthy();
   });
 });
--- src/app/shared/tab/tab.component.spec.ts
+++ src/app/shared/tab/tab.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TabComponent } from './tab.component';
+import { By } from '@angular/platform-browser';
 
 describe('TabComponent', () => {
   let component: TabComponent;
@@ -8,7 +8,7 @@ describe('TabComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TabComponent]
+      declarations: [TabComponent],
     });
     fixture = TestBed.createComponent(TabComponent);
     component = fixture.componentInstance;
@@ -17,5 +17,27 @@ describe('TabComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have .hidden class', () => {
+    const element = fixture.debugElement.query(By.css('.hidden'));
+    const element2 = fixture.nativeElement.querySelector('.hidden');
+    const element3 = document.querySelector('.hidden');
+
+    expect(element).toBeTruthy();
+    expect(element2).toBeTruthy();
+    expect(element3).toBeTruthy();
+  });
+
+  it('should NOT have .hidden class', () => {
+    component.active = true;
+    fixture.detectChanges();
+    const element = fixture.debugElement.query(By.css('.hidden'));
+    const element2 = fixture.nativeElement.querySelector('.hidden');
+    const element3 = document.querySelector('.hidden');
+
+    expect(element).not.toBeTruthy();
+    expect(element2).not.toBeTruthy();
+    expect(element3).not.toBeTruthy();
   });
 });
